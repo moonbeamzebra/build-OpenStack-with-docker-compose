e553cb2ce93826c3719ccd273b94d0211772dba4
jplaberge <moonbeamzebra@magenta.ca>
2026-02-21 07:59:23 -0500

    remove run scripts not needed anymore

--- keystone-run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash +ex
-memcached -u memcache & 
-#apachectl -D FOREGROUND
-service apache2 restart
-touch /var/log/apache2/keystone_access.log
-tail -F /var/log/apache2/keystone_access.log
-#memcached -u memcache & 
-#/usr/sbin/apache2ctl -D FOREGROUND
-
